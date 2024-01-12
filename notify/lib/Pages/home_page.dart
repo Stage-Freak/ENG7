c9e9b0752bd1dd5adf560468c9b9b43ce3516448
Thekaviji <awasthikaviraj21@gmail.com>
2024-01-12 17:43:07 +0545

    remaining date and time added in both collector and thrower pages

--- notify/lib/Pages/home_page.dart
+++ notify/lib/Pages/home_page.dart
@@ -82,110 +82,146 @@ class _HomePageState extends State<HomePage> {
                         ),
                       ),
                     ),
-                    StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-                      stream: FirebaseFirestore.instance
-                          .collection('Collector')
-                          .orderBy('pickupDateTime', descending: true)
-                          .limit(1)
-                          .snapshots(),
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState == ConnectionState.waiting) {
-                          return const CircularProgressIndicator(); // Show loading indicator
-                        } else if (snapshot.hasError) {
-                          return Center(child: Text('Error: ${snapshot.error}'));
-                        } else if (!snapshot.hasData ||
-                            snapshot.data?.docs.isEmpty == true) {
-                          return const Center(
-                              child: Padding(
-                                padding: EdgeInsets.only(top: 50.0),
-                                child: Column(
-                                  children: [
-                                    Image(
-                                      image: AssetImage('assets/images/Schedule.png'),
-                                      width: 100,
-                                      height: 100,
-                                    ),
-                                    Text(
-                                      'No Pickup Scheduled currently.',
-                                      style: TextStyle(
-                                          fontFamily: 'Judson',
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 20),
-                                    ),
-                                  ],
-                                ),
-                              ));
-                        } else {
-                          final collector = snapshot.data!.docs.first;
-                          var pickupDateTime = collector.data()['pickupDateTime'];
-                          final additionalData =
-                          collector.data()['additionalData'];
-        
-                          if (pickupDateTime is Timestamp) {
-                            return Expanded(
-                              child: ListView(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.all(20.0),
-                                    child: Column(
-                                      children: [
-                                        Align(
-                                          alignment: Alignment.centerLeft,
-                                          child: Text(
-                                            'Scheduled Date: ${additionalData['Date']}',
-                                            style: const TextStyle(
-                                              fontWeight: FontWeight.w900,
-                                              fontSize: 18,
-                                              fontFamily: 'Judson',
-                                            ),
-                                          ),
-                                        ),
-                                        const SizedBox(
-                                          height: 10,
-                                        ),
-                                        Align(
-                                          alignment: Alignment.centerLeft,
-                                          child: Text(
-                                            'Scheduled Time: ${additionalData['Time']}',
-                                            style: const TextStyle(
-                                              fontWeight: FontWeight.w900,
-                                              fontSize: 18,
-                                              fontFamily: 'Judson',
-                                            ),
-                                          ),
-                                        ),
-                                        const SizedBox(
-                                          height: 20,
-                                        ),
-                                        Align(
-                                          alignment: Alignment.centerLeft,
-                                          child: Text(
-                                            'Remaining time for pickup: ${additionalData['Time']}',
-                                            style: const TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 18,
-                                              fontFamily: 'Judson',
-                                            ),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
+                StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                  stream: FirebaseFirestore.instance
+                      .collection('Collector')
+                      .orderBy('pickupDateTime', descending: true)
+                      .limit(1)
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const CircularProgressIndicator(); // Show loading indicator
+                    } else if (snapshot.hasError) {
+                      return Center(child: Text('Error: ${snapshot.error}'));
+                    } else if (!snapshot.hasData || snapshot.data?.docs.isEmpty == true) {
+                      return const Center(
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 50.0),
+                          child: Column(
+                            children: [
+                              Image(
+                                image: AssetImage('assets/images/Schedule.png'),
+                                width: 100,
+                                height: 100,
                               ),
-                            );
-                          } else {
-                            return const Center(
-                                child:  Text('No pickup scheduled currently!', style: const TextStyle(
+                              Text(
+                                'No Pickup Scheduled currently.',
+                                style: TextStyle(
                                   fontFamily: 'Judson',
                                   fontWeight: FontWeight.bold,
                                   fontSize: 20,
-                                )));
-                          }
-                        }
-                      },
-                    ),
-                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    } else {
+                      final collector = snapshot.data!.docs.first;
+                      var pickupDateTime = collector.data()['pickupDateTime'];
+                      final additionalData = collector.data()['additionalData'];
+
+                      print('pickupDateTime: $pickupDateTime');
+                      print('additionalData: $additionalData');
+
+                      if (pickupDateTime is Timestamp) {
+                        String dateStr = additionalData['Date'];
+                        String timeStr = additionalData['Time'];
+
+                        DateTime scheduledDate = DateFormat('yyyy-MM-dd').parse(dateStr);
+                        DateTime scheduledTime = DateFormat('HH:mm').parse(timeStr);
+                        DateTime now = DateTime.now();
+
+                        Duration remainingDuration = scheduledDate.difference(now);
+                        Duration remainingTimeDuration = scheduledTime.difference(now);
+
+                        int remainingDay = remainingDuration.inDays;
+                        int remainingHour = remainingTimeDuration.inHours.remainder(24);
+
+                        print('Scheduled Date: $scheduledDate');
+                        print('Current DateTime: $now');
+                        print('Remaining Days: $remainingDay');
+                        print('Remaining Hours: $remainingHour');
+
+                        return Expanded(
+                          child: ListView(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: Column(
+                                  children: [
+                                    Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Text(
+                                        'Scheduled Date: ${DateFormat('yyyy-MM-dd').format(scheduledDate)}',
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.w900,
+                                          fontSize: 18,
+                                          fontFamily: 'Judson',
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Text(
+                                        'Scheduled Time: ${DateFormat('HH:mm').format(scheduledTime)}',
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.w900,
+                                          fontSize: 18,
+                                          fontFamily: 'Judson',
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 20,
+                                    ),
+                                    Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Text(
+                                        'Remaining time for pickup:',
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18,
+                                          fontFamily: 'Judson',
+                                        ),
+                                      ),
+                                    ),
+                                    Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Text(
+                                        '$remainingDay days and $remainingHour hours',
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18,
+                                          fontFamily: 'Judson',
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      } else {
+                        return const Center(
+                          child: Text(
+                            'No pickup scheduled currently!',
+                            style: TextStyle(
+                              fontFamily: 'Judson',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        );
+                      }
+                    }
+                  },
+                ),
+                ],
                 ),
               ),
             ),
--- notify/lib/Pages/thrower_home_page.dart
+++ notify/lib/Pages/thrower_home_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:notify/Pages/mapscreen.dart';
 import 'package:notify/Pages/primary_button.dart';
 
@@ -69,46 +70,67 @@ class _ThrowerHomePagweState extends State<ThrowerHomePage> {
                         return const CircularProgressIndicator(); // Show loading indicator
                       } else if (snapshot.hasError) {
                         return Center(child: Text('Error: ${snapshot.error}'));
-                      } else if (!snapshot.hasData ||
-                          snapshot.data?.docs.isEmpty == true) {
+                      } else if (!snapshot.hasData || snapshot.data?.docs.isEmpty == true) {
                         return const Center(
-                            child: Padding(
-                              padding: EdgeInsets.only(top: 50.0),
-                              child: Column(
-                                children: [
-                                  Image(
-                                    image: AssetImage('assets/images/Schedule.png'),
-                                    width: 100,
-                                    height: 100,
+                          child: Padding(
+                            padding: EdgeInsets.only(top: 50.0),
+                            child: Column(
+                              children: [
+                                Image(
+                                  image: AssetImage('assets/images/Schedule.png'),
+                                  width: 100,
+                                  height: 100,
+                                ),
+                                Text(
+                                  'No Pickup Scheduled currently.',
+                                  style: TextStyle(
+                                    fontFamily: 'Judson',
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
                                   ),
-                                  Text(
-                                    'No Pickup Scheduled currently.',
-                                    style: TextStyle(
-                                        fontFamily: 'Judson',
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 20),
-                                  ),
-                                ],
-                              ),
-                            ));
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
                       } else {
                         final collector = snapshot.data!.docs.first;
                         var pickupDateTime = collector.data()['pickupDateTime'];
-                        final additionalData =
-                        collector.data()['additionalData'];
+                        final additionalData = collector.data()['additionalData'];
+
+                        print('pickupDateTime: $pickupDateTime');
+                        print('additionalData: $additionalData');
 
                         if (pickupDateTime is Timestamp) {
+                          String dateStr = additionalData['Date'];
+                          String timeStr = additionalData['Time'];
+
+                          DateTime scheduledDate = DateFormat('yyyy-MM-dd').parse(dateStr);
+                          DateTime scheduledTime = DateFormat('HH:mm').parse(timeStr);
+                          DateTime now = DateTime.now();
+
+                          Duration remainingDuration = scheduledDate.difference(now);
+                          Duration remainingTimeDuration = scheduledTime.difference(now);
+
+                          int remainingDay = remainingDuration.inDays;
+                          int remainingHour = remainingTimeDuration.inHours.remainder(24);
+
+                          print('Scheduled Date: $scheduledDate');
+                          print('Current DateTime: $now');
+                          print('Remaining Days: $remainingDay');
+                          print('Remaining Hours: $remainingHour');
+
                           return Expanded(
                             child: ListView(
                               children: [
                                 Padding(
-                                  padding: const EdgeInsets.all(16.0),
+                                  padding: const EdgeInsets.all(20.0),
                                   child: Column(
                                     children: [
                                       Align(
                                         alignment: Alignment.centerLeft,
                                         child: Text(
-                                          'Date: ${additionalData['Date']}',
+                                          'Scheduled Date: ${DateFormat('yyyy-MM-dd').format(scheduledDate)}',
                                           style: const TextStyle(
                                             fontWeight: FontWeight.w900,
                                             fontSize: 18,
@@ -122,9 +144,34 @@ class _ThrowerHomePagweState extends State<ThrowerHomePage> {
                                       Align(
                                         alignment: Alignment.centerLeft,
                                         child: Text(
-                                          'Time: ${additionalData['Time']}',
+                                          'Scheduled Time: ${DateFormat('HH:mm').format(scheduledTime)}',
                                           style: const TextStyle(
                                             fontWeight: FontWeight.w900,
+                                            fontSize: 18,
+                                            fontFamily: 'Judson',
+                                          ),
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        height: 20,
+                                      ),
+                                      Align(
+                                        alignment: Alignment.centerLeft,
+                                        child: Text(
+                                          'Remaining time for pickup:',
+                                          style: const TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 18,
+                                            fontFamily: 'Judson',
+                                          ),
+                                        ),
+                                      ),
+                                      Align(
+                                        alignment: Alignment.centerLeft,
+                                        child: Text(
+                                          '$remainingDay days and $remainingHour hours',
+                                          style: const TextStyle(
+                                            fontWeight: FontWeight.bold,
                                             fontSize: 18,
                                             fontFamily: 'Judson',
                                           ),
@@ -138,11 +185,15 @@ class _ThrowerHomePagweState extends State<ThrowerHomePage> {
                           );
                         } else {
                           return const Center(
-                              child:  Text('No pickup scheduled currently!', style: const TextStyle(
+                            child: Text(
+                              'No pickup scheduled currently!',
+                              style: TextStyle(
                                 fontFamily: 'Judson',
                                 fontWeight: FontWeight.bold,
                                 fontSize: 20,
-                              )));
+                              ),
+                            ),
+                          );
                         }
                       }
                     },
